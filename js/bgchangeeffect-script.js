7314ae7b6f52afc73df0a952338d53bac2ee7db9
“Swapnil <swapnilkumbhar366@gmail.com>
2023-12-26 22:21:12 +0530

    first commit

---
+++ js/bgchangeeffect-script.js
@@ -0,0 +1,19 @@
+const imgBox = document.querySelector('.img-box');
+const imgWrap = document.querySelector('.img-wrap');
+const originalImg = document.getElementById('originalimg');
+const line = document.getElementById('line');
+
+
+originalImg.style.width = imgBox.offsetWidth + "px";
+
+let leftSpace = imgBox.offsetLeft;
+
+imgBox.onmousemove = function (e) {
+  let boxWidth = (e.pageX - leftSpace) + "px";
+  imgWrap.style.width = boxWidth;
+  line.style.left = boxWidth;
+}
+
+
+
+
